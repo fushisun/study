31e44b903b86a0c95b79259869cfa2024ad9ae5b
fushi <sunzhiqi0987@163.com>
2023-02-05 18:38:03 +0800

    3

--- Python/venv_test/Untitled-1.py
+++ Python/venv_test/Untitled-1.py
@@ -3,3 +3,16 @@ c="asdfgh"
 b=dict(zip(c,a))
 print(sorted(a,reverse=True))
 print("c">"ba")
+try:
+    1/0
+except ZeroDivisionError:
+    print(21)
+
+
+import time
+time.sleep(3)
+
+import test.asd as asd
+asd.text(3,4)
+import test.abc as abc
+abc.text2()
---
+++ Python/venv_test/test/asd.py
@@ -0,0 +1,5 @@
+def text(a,b):
+    print(a+b)
+    
+if __name__ == "__main__" :
+    text(3,1)
